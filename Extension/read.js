62c72c4f90309cf61b2a83fe6ba3e4991b508e7f
Matt Cooper <matthew_cooper@brown.edu>
2015-11-07 05:22:39 -0500

    got parsing working for main post text. Lots of redundancy because to get lower posts I just called it on scroll. Need to sort out images/links from text.

--- Extension/read.js
+++ Extension/read.js
@@ -1,18 +1,28 @@
 //document.body.style.background = 'yellow';
 
 //get innertext
-function getText(){
-	    $("._1dwg").ready(function() {
-	    	$("._1dwg").css("background", "red");
-	    	alert("here!");
-	    });
-	
+function changeDivs(){
+	$("._1dwg").ready(function() {
+		$("._1dwg").css("background", "red");
+		alert("here!");
+	});
 }
 
 // get whole html 
 function getHTML(){
     return document.body.outerHTML;
 }
-setTimeout(getText(), 5000);
-           //Gives you all the text on the page
-//alert(getHTML());             //Gives you the whole HTML of the page
+
+//Gives you all the text on the page
+//console.log(getHTML());             //Gives you the whole HTML of the page
+
+//._5pbx .userContent -> p
+
+var posts = [];
+
+$(window).scroll(function() {
+	$("._5pbx").find("p").each(function() {
+		console.log($(this).html());
+		posts += $(this).html();
+	});
+});
