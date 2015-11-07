fd23e3dd84cd346f61d1718a1993e6544fdc389a
Matt Cooper <matthew_cooper@brown.edu>
2015-11-07 10:01:09 -0500

    Cleaned up the text and eliminated redundancy by checking against a list of previously posted IDs.

--- Extension/read.js
+++ Extension/read.js
@@ -19,10 +19,75 @@ function getHTML(){
 //._5pbx .userContent -> p
 
 var posts = [];
+var postedIDs = [];
 
 $(window).scroll(function() {
-	$("._5pbx").find("p").each(function() {
-		console.log($(this).html());
-		posts += $(this).html();
+	$("._1dwg").each(function() {//each timeline post
+		var post = {};
+		var type = "";
+		var ID = "";
+		var postText = "";
+		
+		var types = ["posts", "comments", "videos", "photos", "permalink"];//more?
+		$(this).find("._5pcq").each(function() {//get type and ID from permalink
+			//should only be one
+			link = $(this).attr("href");
+			
+			linkElements = link.split("/");
+			for (var i = 0; i < linkElements.length; i++) {
+				if ($.inArray(linkElements[i], types) >= 0) {
+					type = linkElements[i];
+					if (i < linkElements.length - 1) {
+						ID = linkElements[i+1];
+					}
+				}
+			}
+		});
+		$(this).find("._5pbx").each(function() {//get main post text
+			//should only be one
+			postText = "";
+			$(this).find("p").each(function() {
+				postText += cleanHTML($(this).html())+" ";
+			});
+		});
+//		console.log("type: "+type);
+//		console.log("ID  : "+ID);
+//		console.log("post: "+postText);
+//		console.log("");
+		
+		if (type !== "" && ID !== "" && postText !== "" && $.inArray((type, ID), postedIDs) < 0) {
+			console.log("A");
+			post["type"] = type;
+			post["ID"] = ID;
+			post["text"] = postText;
+			
+			posts.push(post);
+			postedIDs.push((type, ID));
+		}
+		
 	});
+	
+	console.log(posts);
+	
+//	$("._5pbx").find("p").each(function() {
+//		var cleanText = cleanHTML($(this).html());
+//		console.log(cleanText);
+//		posts += cleanText;
+//	});
 });
+//get link (use as ID) from ._5pcq -> href.
+
+//Text cleaning
+function cleanHTML(html) {
+	var cleanText = removeElements(html, "a");
+	cleanText = removeElements(cleanText, "br");
+	cleanText = removeElements(cleanText, "span");
+	cleanText = removeElements(cleanText, "i");
+	cleanText = removeElements(cleanText, "u");
+	return cleanText;
+}
+function removeElements(text, selector) {
+    var wrapped = $("<div>" + text + "</div>");
+    wrapped.find(selector).remove();
+    return wrapped.html();
+}
