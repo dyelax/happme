a20b601874b541139b2ac06bf241bce34a9c54a0
Matt Cooper <matthew_cooper@brown.edu>
2015-11-07 02:50:52 -0500

    Nice lookin shiz, plus some test server calls

--- Extension/js/communicator.js
+++ Extension/js/communicator.js
@@ -1,13 +1,23 @@
 $(document).ready(function() {	
 	$('#filterSwitch').click(function() {
-	    if (!$(this).is(':checked')) {			
-		    alert("unchecked");
+	    if ($(this).is(":checked")) {			
+		    console.log("checked");
 	    } else {
-			alert("checked");
+			console.log("unchecked");
 		}
+		
+		var testInfo = {"UID" : 1, "checked": $(this).checked}
+		$.post("url", testInfo, function(data, textStatus) {
+			console.log("Response Data: "+data);
+		});
 	});
 	
-	$("#contactEmail").onchange(function() {
+	$("#contactEmail").change(function() {
+		//test post
+		var testInfo = {"UID" : 1, "contact": $(this).val}
+		$.post("url", testInfo, function(data, textStatus) {
+			console.log("Response Data: "+data);
+		});
 		console.log("changed");
 	});
 });
