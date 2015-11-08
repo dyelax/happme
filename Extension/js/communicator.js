662f9267df16bc8fec126fea4ac11fea97b0df29
Matt Cooper <matthew_cooper@brown.edu>
2015-11-08 01:26:58 -0500

    lotsashit

--- Extension/js/communicator.js
+++ Extension/js/communicator.js
@@ -1,34 +1,142 @@
 var uid;
 var contactEmail = "";
 var shouldFilter = false;
-var serverURL = "http://happme.azurewebsites.net";
+var serverURL = "http://happme.azurewebsites.net/";
 
-$(document).ready(function() {		
-	$("#contactEmail").val(contactEmail);
+$(document).ready(function() {			
+	chrome.storage.sync.get("contactEmail", function(object) {
+		console.log(object);
+		contactEmail = object["contactEmail"];
+		getShouldFilter();
+	});
 		
+	$("#triggers li input").click(function() {
+		saveTriggers();
+	});
 	$('#filterSwitch').click(function() {
-		alert("A");
-
 	    if ($(this).is(":checked")) {			
 		    console.log("checked");
 	    } else {
 			console.log("unchecked");
 		}
 		
+		url = serverURL + "";
 		var info = {"uid" : uid, "checked": $(this).is(":checked")}
-		$.post(serverURL, info, function(data, textStatus) {
+		$.post(url, info, function(data, textStatus) {
 			console.log("Response Data: "+data);
+		});
+		
+		chrome.storage.sync.set({"shouldFilter" : $(this).is(":checked")}, function() {
+			console.log("Successfully stored filter");
 		});
 	});
 	
 	$("#contactEmail").change(function() {
-		var info = {"uid" : uid, "contact_email": $(this).val}
-		$.post(serverURL, info, function(data, textStatus) {
+		url = serverURL + "change_contact";
+		var info = {"uid" : "mattjcoop", "contact_email": $(this).val()}
+		$.post(url, info, function(data, textStatus) {
 			console.log("Response Data: "+data);
 		});
 		console.log("changed");
+		
+		chrome.storage.sync.set({"contactEmail" : $(this).val()}, function() {
+			console.log("Successfully stored contact email");
+		});
 	});
 });
+
+function getShouldFilter(){
+	chrome.storage.sync.get("shouldFilter", function(object) {
+		console.log("filter:");
+		console.log(object);
+		shouldFilter = object["shouldFilter"];
+		
+		getTriggers();
+	});
+}
+
+function getTriggers() {
+	getProfanity();
+}
+function getProfanity(){
+	chrome.storage.sync.get("profanity", function(object) {
+		$("#profanity").prop("checked", object["profanity"]);
+		getPTSD();
+	}); 
+}
+function getPTSD(){
+	chrome.storage.sync.get("ptsd", function(object) {
+		$("#PTSD").prop("checked", object["ptsd"]);
+		getDrugUse();
+	}); 
+}
+function getDrugUse(){
+	chrome.storage.sync.get("drugUse", function(object) {
+		$("#drugUse").prop("checked", object["drugUse"]);
+		getSexualAssault();
+	}); 
+}
+function getSexualAssault(){
+	chrome.storage.sync.get("sexualAssault", function(object) {
+		$("#sexualAssault").prop("checked", object["sexualAssault"]);
+		getChildAbuse();
+	}); 
+}
+function getChildAbuse(){
+	chrome.storage.sync.get("childAbuse", function(object) {
+		$("#childAbuse").prop("checked", object["childAbuse"]);
+		getSelfharm();
+	}); 
+}
+function getSelfharm(){
+	chrome.storage.sync.get("self-harm", function(object) {
+		$("#self-harm").prop("checked", object["self-harm"]);
+		setupPopup();
+	}); 
+}
+
+function saveTriggers() {
+//	$("#triggers ul li input").each(function() {
+//		var name = $(this).attr("name");
+//		var isChecked = $(this).is(":checked");
+//		
+//		console.log(isChecked);
+//		var info = {};
+//		info[name] = isChecked;
+//		chrome.storage.sync.set(info, function() {
+//			console.log("set checked for "+ name +" successful.");
+//		});
+//	});
+	
+	chrome.storage.sync.set({"profanity" : $("#profanity").is(":checked")}, function() {
+		console.log("success");
+	});
+	chrome.storage.sync.set({"ptsd" : $("#PTSD").is(":checked")}, function() {
+		console.log("success");
+	});
+	chrome.storage.sync.set({"drugUse" : $("#drugUse").is(":checked")}, function() {
+		console.log("success");
+	});
+	chrome.storage.sync.set({"sexualAssault" : $("#sexualAssault").is(":checked")}, function() {
+		console.log("success");
+	});
+	chrome.storage.sync.set({"childAbuse" : $("#childAbuse").is(":checked")}, function() {
+		console.log("success");
+	});
+	chrome.storage.sync.set({"self-harm" : $("#self-harm").is(":checked")}, function() {
+		console.log("success");
+	});
+							
+}
+
+function setupPopup() {
+	$("#contactEmail").val(contactEmail);
+	$("#filterSwitch").prop("checked", shouldFilter);
+	
+	if (!contactEmail) {
+		contactEmail = "";
+	}
+}
 
 window.onload = function() {
 	console.log("onload" + Date());
@@ -38,8 +146,8 @@ function setUser(newUID) {
 	uid = newUID;
 	
 	//pull contactEmail and shouldFilter from server
-	$.get(serverURL, {"uid" : uid}, function(data) {
-		contactEmail = data["contact_email"];
-		shouldFilter = data["filtering"];
-	});
+//	$.get(serverURL, {"uid" : uid}, function(data) {
+//		contactEmail = data["contact_email"];
+//		shouldFilter = data["filtering"];
+//	});
 }
--- Extension/read.js
+++ Extension/read.js
@@ -1,12 +1,12 @@
 //document.body.style.background = 'yellow';
 
 //get innertext
-function changeDivs(){
-	$("._1dwg").ready(function() {
-		$("._1dwg").css("background", "red");
-		alert("here!");
-	});
-}
+//function changeDivs(){
+//	$("._1dwg").ready(function() {
+//		$("._1dwg").css("background", "red");
+//		alert("here!");
+//	});
+//}
 
 // get whole html of page
 function getHTML(){
@@ -21,21 +21,27 @@ $(window).load(function(){
 	var userURL = $("._36he").attr("href");
 	userID = userURL.substr(25, userURL.indexOf("?") - 25);//cut off facebook domain and GET info.
 	setUID(userID);
+	
+	scanFeed();
 });
 
 $(window).scroll(function() {
+	scanFeed();
+});
+
+function scanFeed(){
 	$("._5v3q").each(function() {//each timeline post
 		var post = {};
 		var type = "";
 		var ID = "";
 		var postText = "";
 		var commentText = "";
-		
+
 		var types = ["posts", "comments", "videos", "photos", "permalink"];//more?
 		$(this).find("._5pcq").each(function() {//get type and ID from permalink
 			//should only be one
 			link = $(this).attr("href");
-			
+
 			linkElements = link.split("/");
 			for (var i = 0; i < linkElements.length; i++) {
 				if ($.inArray(linkElements[i], types) >= 0) {
@@ -57,27 +63,38 @@ $(window).scroll(function() {
 				commentText += cleanHTML($(this).html())+". ";
 			});
 		});
-		
+
 		if (type !== "" && ID !== "" && postText !== "" && $.inArray(postText+commentText, postedText) < 0) {	
 			post["uid"] = userID;
 			post["filtering"] = false;//TODO
 			post["post_type"] = type;
 			post["post_ID"] = ID;
 			post["contents"] = {"post" : postText, "comment" : commentText};
-						
+			
+			var triggers = [];
+			$("#triggers ul li input").each(function() {
+				if ($(this).is(":checked")){
+					triggers.push($(this).attr("id"));
+				}
+			});
+			
+			var triggerString = triggers + "";
+			
+			console.log(triggerString);
+
 			//POST the post to the server
-			var serverURL = "http://happme.azurewebsites.net";
+			var serverURL = "http://happme.azurewebsites.net/record_story";
 			$.post(serverURL, post, function(data, textStatus) {
 				console.log("Response Data: "+data);
 			});
-			
+
 			postedText.push(postText+commentText);
 			posts.push(post);
 		}
 	});
 	
 	console.log(posts);
-});
+}
 
 //Text cleaning
 function cleanHTML(html) {
